0c23da360b6723c13abe08e4cf0e1a343f6b2ec3
tenchooooo <tenchooo.227@gmail.com>
2022-04-20 12:10:02 +0000

    [Finish]5-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -14,14 +14,14 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment =PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
